47b2637e5877c468285703856f6d1712301c52c6
AriasVictoria <131209228+AriasVictoria@users.noreply.github.com>
2023-05-12 20:05:20 -0300

    Diagrama de clases

--- kisoco1/bibloteca/Vendedor.cs
+++ kisoco1/bibloteca/Vendedor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace bibloteca
 {
-    public class Vendedor 
+    public class Vendedor
     {
         public int Id { get; set; }
         public Vendedor usuariovendedor{ get; set; }
